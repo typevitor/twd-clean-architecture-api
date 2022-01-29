11cbc6c386eb2ee3cbc66f8021dabc22deb5a17d
Vitor Leal <vitorlealeng@gmail.com>
2022-01-29 08:30:20 -0300

    test: add, find and check users existence

--- tests/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ tests/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -8,4 +8,14 @@ describe('In Memory User Repository', () => {
     const user = await userRepo.findUserByEmail('any@email.com')
     expect(user).toBeNull()
   })
+
+  test('should return user if it is found in repository', async () => {
+    const users: UserData[] = []
+    const name: string = 'any_name'
+    const email: string = 'any@email.com'
+    const userRepo = new InMemoryUserRepository(users)
+    await userRepo.add({ name, email })
+    const user = await userRepo.findUserByEmail('any@email.com')
+    expect(user.name).toBe('any_name')
+  })
 })
