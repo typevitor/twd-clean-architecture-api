cd3a7187a042b33c5d7898c8049fb481509d61ab
Vitor A Leal <vitorlealeng@gmail.com>
2022-02-04 07:24:49 -0300

    test: validate empty and null emails

---
+++ tests/entities/email-validation.spec.ts
@@ -0,0 +1,19 @@
+// import { Email } from '@/entities/email-validation'
+import { Email } from '../../src/entities/email-validation'
+
+describe('Email Validation', () => {
+  test('Should not accept null strings', () => {
+    const email = null
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('Should not accept empty strings', () => {
+    const email: string = ''
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('Should return true when valid email is provided', () => {
+    const email: string = 'valid_email@mail.com'
+    expect(Email.validate(email)).toBeTruthy()
+  })
+})
