badda351161db9fe5ef8a01c1aea071bb6994216
Vitor Leal <vitorlealeng@gmail.com>
2022-02-05 08:18:56 -0300

    feat: validate email with an invalid char

--- src/entities/email-validation.ts
+++ src/entities/email-validation.ts
@@ -1,11 +1,13 @@
 export class Email {
   static validate (email: string): boolean {
+    const EMAIL_REGEX = /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
     if (!email) {
       return false
     }
     if (email.length > 320) {
       return false
     }
+    if (!EMAIL_REGEX.test(email)) return false
     const [localpart, domain] = email.split('@')
     if (localpart.length === 0 || localpart.length > 64) return false
     if (domain.length === 0 || domain.length > 255) return false
