068fb9e7335805b87023d1322300612acb77e0e4
Vitor Leal <vitorlealeng@gmail.com>
2022-02-05 08:07:55 -0300

    feat: validate emails with domain parts larger than 64 chars

--- src/entities/email-validation.ts
+++ src/entities/email-validation.ts
@@ -9,6 +9,8 @@ export class Email {
     const [localpart, domain] = email.split('@')
     if (localpart.length === 0 || localpart.length > 64) return false
     if (domain.length === 0 || domain.length > 255) return false
+    const domainParts = domain.split('.')
+    if (domainParts.some(part => part.length > 63)) return false
     return true
   }
 }
