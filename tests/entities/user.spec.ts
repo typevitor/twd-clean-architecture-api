d35a3ce489271da494fe3c753056e65c0eaca58c
Vitor A Leal <vitorlealeng@gmail.com>
2022-02-15 07:15:24 -0300

    test: ensure return InvalidNameError when an invalid name length is provided

--- tests/entities/user.spec.ts
+++ tests/entities/user.spec.ts
@@ -1,11 +1,17 @@
 import { User } from '@/entities/user'
 import { left } from '@/shared/either'
-import { InvalidEmailError } from '@/entities/errors/invalid-email-error'
+import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
 
 describe('User domain entity', () => {
   test('Should not create user with an invalid e-mail address', () => {
     const invalidEmail = 'invalid_emailmail.com'
     const userOrError = User.create({ name: 'any_name', email: invalidEmail })
     expect(userOrError).toEqual(left(new InvalidEmailError()))
+  })
+
+  test('Should not create user with a name less than 2 chars', () => {
+    const invalidName = 'D      '
+    const userOrError = User.create({ name: invalidName, email: 'valid@mail.com' })
+    expect(userOrError).toEqual(left(new InvalidNameError()))
   })
 })
