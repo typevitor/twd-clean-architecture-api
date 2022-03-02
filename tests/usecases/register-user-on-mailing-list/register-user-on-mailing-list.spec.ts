02777bc8f9b99088adc31c8fd5cf84e68d135182
Vitor A Leal <vitorlealeng@gmail.com>
2022-03-02 07:30:58 -0300

    test: prevent to add user with an invalid email

--- tests/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ tests/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -2,11 +2,12 @@ import { UserData } from '@/entities/user-data'
 import { UserRepository } from './ports/user-repository'
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list/register-user-on-mailing-list'
 import { InMemoryUserRepository } from '@/usecases/register-user-on-mailing-list/repository/in-memory-user-repository'
+import { InvalidEmailError } from '@/entities/errors'
+import { left } from '@/shared/either'
 
 describe('Register user on mailing list use case', () => {
   test('should add user with complete data to mailing list', async () => {
     const users: UserData[] = []
-    console.log(users)
     const repo: UserRepository = new InMemoryUserRepository(users)
     const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
     const name = 'any_name'
@@ -15,5 +16,17 @@ describe('Register user on mailing list use case', () => {
     const user = repo.findUserByEmail('any_email@email.com')
     expect((await user).name).toBe('any_name')
     expect(response.value.name).toBe('any_name')
+  })
+
+  test('should not add user with and invalid_email', async () => {
+    const users: UserData[] = []
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    const name = 'any_name'
+    const invalidEmail = 'invalid_email.@email.com'
+    const response = await usecase.registerUserOnMailingList({ name, email: invalidEmail })
+    const user = repo.findUserByEmail(invalidEmail)
+    expect((await user)).toBeNull()
+    expect(response).toEqual(left(new InvalidEmailError()))
   })
 })
