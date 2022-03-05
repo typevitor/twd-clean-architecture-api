26e73c37da2b9cff66aae66dab23bbb564458df0
Vitor Leal <vitorlealeng@gmail.com>
2022-03-05 07:50:15 -0300

    feat: web controller should return 400 when invalid user data is provided

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -1,7 +1,7 @@
 import { UserData } from '@/entities'
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list/register-user-on-mailing-list'
 import { HttpRequest, HttpResponse } from './ports'
-import { created } from '@/web-controllers/utils/http-helper'
+import { created, badrequest } from '@/web-controllers/utils/http-helper'
 
 export class RegisterUserController {
   private readonly usecase: RegisterUserOnMailingList
@@ -13,6 +13,9 @@ export class RegisterUserController {
   async handle (request: HttpRequest): Promise<HttpResponse> {
     const userData: UserData = request.body
     const response = await this.usecase.registerUserOnMailingList(userData)
+    if (response.isLeft()) {
+      return badrequest(response.value)
+    }
     if (response.isRight()) {
       return created(response.value)
     }
--- src/web-controllers/utils/http-helper.ts
+++ src/web-controllers/utils/http-helper.ts
@@ -4,3 +4,8 @@ export const created = (data: any): HttpResponse => ({
   statusCode: 201,
   body: data
 })
+
+export const badrequest = (data: any): HttpResponse => ({
+  statusCode: 400,
+  body: data
+})
