eb5deace28f7c0120af75c7667fd1bcc9875d7f5
Vitor A Leal <vitorlealeng@gmail.com>
2022-03-10 07:32:38 -0300

    test: apply content-type middleware

---
+++ tests/main/middlewares/content-type.test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+
+describe('Content-type middleware', () => {
+  test('should set default application/JSON content-type', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+
+  test('should set content-type XML when forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send()
+    })
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
+})
