5bc7220285cc56ee5d43c106a1b8c7514c0cfa4a
Vitor A Leal <vitorlealeng@gmail.com>
2022-03-10 07:25:35 -0300

    feat: enable cors middleware

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { bodyParser } from '@/main/config/middlewares/body-parser'
+import { cors } from '@/main/config/middlewares/cors'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
