6afe052b87f48c1d68edf751cd9c50f5bce356b5
Vitor Leal <vitorlealeng@gmail.com>
2022-03-12 07:59:31 -0300

    feat: add config register route

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import setUpMiddleware from '@/main/config/middleware'
+import setupRoutes from './routes'
 
 const app = express()
 setUpMiddleware(app)
+setupRoutes(app)
 
 export default app
