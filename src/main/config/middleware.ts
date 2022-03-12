92f9f3a252e7937aef563a90cde3f1e2a9563a36
Vitor Leal <vitorlealeng@gmail.com>
2022-03-12 07:14:52 -0300

    refactor: organize imports

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,7 +1,5 @@
 import { Express } from 'express'
-import { bodyParser } from '@/main/config/middlewares/body-parser'
-import { cors } from '@/main/config/middlewares/cors'
-import { contentType } from '@/main/config/middlewares/content-type'
+import { bodyParser, cors, contentType } from '@/main/config/middlewares'
 
 export default (app: Express): void => {
   app.use(bodyParser)
