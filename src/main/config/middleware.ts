5fabd4459c50ae2c6e1bbcff7ad8a18a041ab5ea
Vitor Leal <vitorlealeng@gmail.com>
2022-03-12 07:18:13 -0300

    Revert "refactor: organize imports"
    This reverts commit 92f9f3a252e7937aef563a90cde3f1e2a9563a36.

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,5 +1,7 @@
 import { Express } from 'express'
-import { bodyParser, cors, contentType } from '@/main/config/middlewares'
+import { bodyParser } from '@/main/config/middlewares/body-parser'
+import { cors } from '@/main/config/middlewares/cors'
+import { contentType } from '@/main/config/middlewares/content-type'
 
 export default (app: Express): void => {
   app.use(bodyParser)
