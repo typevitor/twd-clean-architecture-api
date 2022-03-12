40f0420cb640a3a3253653cf807e3e0d95d2505b
Vitor Leal <vitorlealeng@gmail.com>
2022-03-12 07:59:42 -0300

    test: add config register route

---
+++ tests/main/config/routes/register-route.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+
+describe('Register route', () => {
+  test('should return an account on sucess', async () => {
+    app.post('/test_cors', (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .post('/api/register')
+      .send({
+        name: 'Any Name',
+        email: 'anymail@mail.com'
+      })
+      .expect(201)
+  })
+})
