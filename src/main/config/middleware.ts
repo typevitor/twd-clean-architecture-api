9c778692385735c7a7b8ffaa8f562831fec29e30
Vitor A Leal <vitorlealeng@gmail.com>
2022-03-14 07:21:07 -0300

    refactor: move middleware, routes and adapters to inside main folder

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,7 +1,7 @@
 import { Express } from 'express'
-import { bodyParser } from '@/main/config/middlewares/body-parser'
-import { cors } from '@/main/config/middlewares/cors'
-import { contentType } from '@/main/config/middlewares/content-type'
+import { bodyParser } from '@/main/middlewares/body-parser'
+import { cors } from '@/main/middlewares/cors'
+import { contentType } from '@/main/middlewares/content-type'
 
 export default (app: Express): void => {
   app.use(bodyParser)
--- src/main/routes/register-route.ts
+++ src/main/routes/register-route.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { makeRegisterUserController } from '@/main/factories/register'
-import { adaptRoute } from '@/main/config/adapters/express-route-adapter'
+import { adaptRoute } from '@/main/adapters/express-route-adapter'
 
 export default (router: Router): void => {
   router.post('/register', adaptRoute(makeRegisterUserController()))
