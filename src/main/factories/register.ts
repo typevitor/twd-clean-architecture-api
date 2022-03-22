e710c31a4a96062e2add06b3ae867a3dc93089a1
Vitor A Leal <vitorlealeng@gmail.com>
2022-03-22 07:24:58 -0300

    refactor: change user repository to monbodb on register factory

--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,10 +1,10 @@
+import { MongoDbUserRepository } from '@/infra/repositories/mongodb'
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list/register-user-on-mailing-list'
-import { InMemoryUserRepository } from '@/usecases/register-user-on-mailing-list/repository/in-memory-user-repository'
 import { RegisterUserController } from '@/web-controllers'
 
 export const makeRegisterUserController = (): RegisterUserController => {
-  const inMemoryUserRepository = new InMemoryUserRepository([])
-  const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(inMemoryUserRepository)
+  const mongoDbUserRepository = new MongoDbUserRepository()
+  const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(mongoDbUserRepository)
   const registerUserController: RegisterUserController = new RegisterUserController(registerUserOnMailingListUseCase)
   return registerUserController
 }
