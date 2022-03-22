82233c742d09deb6df9cc8f6cef98cbeaf5e93bb
Vitor A Leal <vitorlealeng@gmail.com>
2022-03-22 07:31:06 -0300

    refactor: connect to mongodb on server startup

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,12 @@
 import 'module-alias/register'
-import app from '@/main/config/app'
+import { MongoHelper } from '@/infra/repositories/mongodb/helpers'
 
-app.listen(3000, () => {
-  console.log('Server running at http://localhost:3000')
-})
+MongoHelper
+  .connect(process.env.MONGO_URL)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(3000, () => {
+      console.log('Server running at http://localhost:3000')
+    })
+  })
+  .catch(console.error)
