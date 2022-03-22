c9e748ef410f674660aacbb9179cfdaeeac390b2
Vitor A Leal <vitorlealeng@gmail.com>
2022-03-22 07:10:47 -0300

    feat: add mongodb user repository

---
+++ src/infra/repositories/mongodb/mongodb-user-repository.ts
@@ -0,0 +1,31 @@
+import { UserData } from '@/entities'
+import { UserRepository } from '@/usecases/register-user-on-mailing-list/ports/user-repository'
+import { MongoHelper } from './helpers'
+
+export class MongoDbUserRepository implements UserRepository {
+  async add (user: UserData): Promise<void> {
+    const userCollection = MongoHelper.getCollection('users')
+    const exists = await this.exists(user)
+    if (!exists) {
+      await userCollection.insertOne(user)
+    }
+  }
+
+  async findUserByEmail (email: string): Promise<UserData> {
+    const userCollection = MongoHelper.getCollection('users')
+    const result = await userCollection.findOne<UserData>({ email })
+    return result
+  }
+
+  findAllUsers (): Promise<UserData[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  async exists (user: UserData): Promise<boolean> {
+    const result = await this.findUserByEmail(user.email)
+    if (result != null) {
+      return true
+    }
+    return false
+  }
+}
